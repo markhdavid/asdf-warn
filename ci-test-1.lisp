9e6b8035aa1c6b1900ed05438d2c60ca8e6ef6ba
Mark David <6209504+markhdavid@users.noreply.github.com>
2018-03-15 00:54:42 -0700

    try again

--- ci-test-1.lisp
+++ ci-test-1.lisp
@@ -1,6 +1,12 @@
 (defconstant *exit-success-value* 0)
 (defconstant *exit-failure-value* 1)
+(defvar *basedir*
+  (make-pathname
+   :defaults *load-pathname*
+   :name nil
+   :type nil
+   ::version nil))
 (asdf:initialize-source-registry   
- `(:source-registry (:tree "./") :inherit-configuration))
+ `(:source-registry (:tree ,*basedir*) :inherit-configuration))
 (asdf:make "my-system")
 (uiop:quit *exit-success-value*)
