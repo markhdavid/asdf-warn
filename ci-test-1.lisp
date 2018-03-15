dfa58bab3305f9827a0ccc6e232bc3f8bc40a64a
Mark David <6209504+markhdavid@users.noreply.github.com>
2018-03-15 00:49:16 -0700

    try again

--- ci-test-1.lisp
+++ ci-test-1.lisp
@@ -1,3 +1,6 @@
 (defconstant *exit-success-value* 0)
 (defconstant *exit-failure-value* 1)
+(asdf:initialize-source-registry   
+ `(:source-registry (:tree "./") :inherit-configuration))
+(asdf:make "my-system")
 (uiop:quit *exit-success-value*)
